b7804f914effb0b7f4cf2f47bb8d25153d98d9f0
Yoanm <4410697+yoanm@users.noreply.github.com>
2026-05-05 11:25:00 +0200

    Improve

--- summary/README.md
+++ summary/README.md
@@ -2,98 +2,9 @@
 
 ## Functions
 
-### func [Generate](./summary.go#L17)
-
-`func Generate(mrkList SectionsMap) string`
-
-### func [GenerateForChanges](./main.go#L14)
+### func [GenerateForChanges](./categorizer.go#L11)
 
 `func GenerateForChanges(changes shared.DiffMap) string`
-
-## Types
-
-### type [CategoriesMap](./types.go#L48)
-
-`type CategoriesMap map[MarkdownCategory]SubCategoriesMap`
-
-### type [ItemsMap](./types.go#L46)
-
-`type ItemsMap map[MarkdownItem]PkgList`
-
-### type [MarkdownCategory](./types.go#L15)
-
-`type MarkdownCategory string`
-
-#### Constants
-
-```golang
-const (
-    ProdUsageCategory    MarkdownCategory = "PROD_USAGE"
-    DevOnlyUsageCategory MarkdownCategory = "DEV_ONLY_USAGE"
-)
-```
-
-### type [MarkdownItem](./types.go#L29)
-
-`type MarkdownItem string`
-
-#### Constants
-
-```golang
-const (
-    UnknownUpdateItem        MarkdownItem = "UNKNOWN_UPDATE"
-    SemverMajorUpgradeItem   MarkdownItem = "SEMVER_MAJOR_UPGRADE"
-    SemverMinorUpgradeItem   MarkdownItem = "SEMVER_MINOR_UPGRADE"
-    SemverPatchUpgradeItem   MarkdownItem = "SEMVER_PATCH_UPGRADE"
-    SemverMajorDowngradeItem MarkdownItem = "SEMVER_MAJOR_DOWNGRADE"
-    SemverMinorDowngradeItem MarkdownItem = "SEMVER_MINOR_DOWNGRADE"
-    SemverPatchDowngradeItem MarkdownItem = "SEMVER_PATCH_DOWNGRADE"
-    RemovalItem              MarkdownItem = "REMOVAL"
-    AdditionItem             MarkdownItem = "ADDITION"
-    SameItem                 MarkdownItem = "SAME"
-)
-```
-
-### type [MarkdownSection](./types.go#L5)
-
-`type MarkdownSection string`
-
-#### Constants
-
-```golang
-const (
-    CautionSection   MarkdownSection = "CAUTION"
-    WarningSection   MarkdownSection = "WARNING"
-    ImportantSection MarkdownSection = "IMPORTANT"
-    TipSection       MarkdownSection = "TIP"
-    NoteSection      MarkdownSection = "NOTE"
-)
-```
-
-### type [MarkdownSubCategory](./types.go#L22)
-
-`type MarkdownSubCategory string`
-
-#### Constants
-
-```golang
-const (
-    RequirementSubCategory MarkdownSubCategory = "REQUIREMENT"
-    TransitiveSubCategory  MarkdownSubCategory = "TRANSITIVE"
-)
-```
-
-### type [PkgList](./types.go#L45)
-
-`type PkgList []*shared.PackageChange`
-
-### type [SectionsMap](./types.go#L49)
-
-`type SectionsMap map[MarkdownSection]CategoriesMap`
-
-### type [SubCategoriesMap](./types.go#L47)
-
-`type SubCategoriesMap map[MarkdownSubCategory]ItemsMap`
 
 ## Sub Packages
 
--- summary/main.go
+++ summary/main.go
@@ -3,273 +3,179 @@ package summary
 import (
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
 
 	"github.com/yoanm/go-deps-diff/shared"
+	"github.com/yoanm/go-deps-diff/summary/markdown"
 )
 
-func GenerateForChanges(changes shared.DiffMap) string {
-	return generate(buildDefaultSectionsMap(changes))
+const (
+	sectionHeaderLevel  = 2
+	categoryHeaderLevel = 3
+)
+
+func generate(mrkList sectionsMap) string {
+	builder := markdown.NewBuilder()
+
+	inOrderMapIteratorHelper[markdownSection, categoriesMap](
+		mrkList,
+		getSectionsOrder(),
+		func(sectionName markdownSection, categoriesMap categoriesMap) {
+			processSection(builder, categoriesMap, sectionName)
+		},
+	)
+
+	processSummaryCaption(builder)
+
+	return builder.String()
 }
 
-func buildDefaultSectionsMap(changes shared.DiffMap) sectionsMap {
-	list := sectionsMap{}
+func processSection(builder *markdown.Builder, categoriesMap categoriesMap, sectionName markdownSection) {
+	fmt.Fprintln(os.Stderr, "Processing section:", sectionName)
+	builder.Header(
+		getSectionHeaderFor(sectionName)+"<br/><sub><sup>"+getSectionDescriptionFor(sectionName)+"</sub></sup>",
+		sectionHeaderLevel,
+		0,
+	)
 
-	for _, change := range changes {
-		categoryType, subCategoryType := getMarkdownCategoryType(change)
-		itemType := getMarkdownItemType(change)
-		sectionType := getMarkdownSectionType(subCategoryType, itemType, change.Package.IsAbandoned())
+	inOrderMapIteratorHelper[markdownCategory, subCategoriesMap](
+		categoriesMap,
+		getCategoriesOrder(),
+		func(categoryName markdownCategory, subCategoriesMap subCategoriesMap) {
+			openedDetails := categoryName == prodUsageCategory &&
+				(cautionSection == sectionName || warningSection == sectionName || importantSection == sectionName)
 
-		switch {
-		case list[sectionType] == nil:
-			list[sectionType] = make(categoriesMap)
+			processCategory(builder, subCategoriesMap, categoryName, openedDetails)
+		},
+	)
 
-			fallthrough
-		case list[sectionType][categoryType] == nil:
-			list[sectionType][categoryType] = make(subCategoriesMap)
-
-			fallthrough
-		case list[sectionType][categoryType][subCategoryType] == nil:
-			list[sectionType][categoryType][subCategoryType] = make(itemsMap)
-
-			fallthrough
-		case list[sectionType][categoryType][subCategoryType][itemType] == nil:
-			list[sectionType][categoryType][subCategoryType][itemType] = pkgList{}
-		}
-
-		// _debugPackageList(sectionType, categoryType, subCategoryType, itemType, change)
-
-		list[sectionType][categoryType][subCategoryType][itemType] = append(
-			list[sectionType][categoryType][subCategoryType][itemType],
-			change,
-		)
-	}
-
-	return list
+	builder.WriteLine("<hr/>", 0)
 }
 
-func getMarkdownSectionType(
-	subCategoryType markdownSubCategory,
-	itemType markdownItem,
-	isAbandoned bool,
-) markdownSection {
-	switch {
-	case isCandidateForCautionSection(subCategoryType, itemType, isAbandoned):
-		return cautionSection
-	case isCandidateForWarningSection(subCategoryType, itemType, isAbandoned):
-		return warningSection
-	case isCandidateForImportantSection(subCategoryType, itemType):
-		return importantSection
-	case isCandidateForTipSection(subCategoryType, itemType):
-		return tipSection
-	}
-
-	//# Note
-	//## Production usage
-	//### Requirements
-	//- SEMVER_PATCH_UPGRADE
-	//- SAME
-	//### Transitives
-	//- REMOVAL
-	//- SEMVER_MINOR_UPGRADE
-	//- SEMVER_PATCH_UPGRADE
-	//- ADDITION
-	//- SAME
-	//## Dev-only usage
-	//### Requirements
-	//- SEMVER_PATCH_UPGRADE
-	//- SAME
-	//### Transitives
-	//- REMOVAL
-	//- SEMVER_MINOR_UPGRADE
-	//- SEMVER_PATCH_UPGRADE
-	//- ADDITION
-	//- SAME
-
-	return noteSection
-}
-
-func isCandidateForCautionSection(subCategoryType markdownSubCategory, itemType markdownItem, isAbandoned bool) bool {
-	//# Caution
-	//## Production usage
-	//### Requirements
-	//- UNKNOWN_UPDATE
-	//- SEMVER_MAJOR_DOWNGRADE
-	//- ADDITION__ABANDONED
-	//### Transitives
-	//## Dev-only usage
-	//### Requirements
-	//- UNKNOWN_UPDATE
-	//- SEMVER_MAJOR_DOWNGRADE
-	//- ADDITION__ABANDONED
-	//### Transitives
-	// -> process prod usage and dev-only usage the same way
-	return subCategoryType == requirementSubCategory &&
-		(itemType == unknownUpdateItem ||
-			itemType == semverMajorDowngradeItem ||
-			(itemType == additionItem && isAbandoned))
-}
-
-func isCandidateForWarningSection(subCategoryType markdownSubCategory, itemType markdownItem, isAbandoned bool) bool {
-	//# Warning
-	//## Production usage
-	//### Requirements
-	//- SEMVER_MAJOR_UPGRADE
-	//- SEMVER_MINOR_DOWNGRADE
-	//### Transitives
-	//- UNKNOWN_UPDATE
-	//- SEMVER_MAJOR_DOWNGRADE
-	//- ADDITION__ABANDONED
-	//## Dev-only usage
-	//### Requirements
-	//- SEMVER_MAJOR_UPGRADE
-	//- SEMVER_MINOR_DOWNGRADE
-	//### Transitives
-	//- UNKNOWN_UPDATE
-	//- SEMVER_MAJOR_DOWNGRADE
-	//- ADDITION__ABANDONED
-	// -> process prod usage and dev-only usage the same way
-	if subCategoryType == requirementSubCategory &&
-		(itemType == semverMajorUpgradeItem || itemType == semverMinorDowngradeItem) {
-		return true
-	}
-
-	return subCategoryType == transitiveSubCategory &&
-		(itemType == unknownUpdateItem ||
-			itemType == semverMajorDowngradeItem ||
-			(itemType == additionItem && isAbandoned))
-}
-
-func isCandidateForImportantSection(subCategoryType markdownSubCategory, itemType markdownItem) bool {
-	// # Important
-	//## Production usage
-	//### Requirements
-	//- SEMVER_PATCH_DOWNGRADE
-	//- REMOVAL
-	//### Transitives
-	//- SEMVER_MAJOR_UPGRADE
-	//- SEMVER_MINOR_DOWNGRADE
-	//## Dev-only usage
-	//### Requirements
-	//- SEMVER_PATCH_DOWNGRADE
-	//- REMOVAL
-	//### Transitives
-	//- SEMVER_MAJOR_UPGRADE
-	//- SEMVER_MINOR_DOWNGRADE
-	// -> process prod usage and dev-only usage the same way
-	if subCategoryType == requirementSubCategory && (itemType == semverPatchDowngradeItem || itemType == removalItem) {
-		return true
-	}
-
-	return subCategoryType == transitiveSubCategory &&
-		(itemType == semverMajorUpgradeItem || itemType == semverMinorDowngradeItem)
-}
-
-func isCandidateForTipSection(subCategoryType markdownSubCategory, itemType markdownItem) bool {
-	// # Tip
-	//## Production usage
-	//### Requirements
-	//- SEMVER_MINOR_UPGRADE
-	//- ADDITION
-	//### Transitives
-	//- SEMVER_PATCH_DOWNGRADE
-	//- REMOVAL
-	//## Dev-only usage
-	//### Requirements
-	//- SEMVER_MINOR_UPGRADE
-	//- ADDITION
-	//### Transitives
-	//- SEMVER_PATCH_DOWNGRADE
-	//- REMOVAL
-	// -> process prod usage and dev-only usage the same way
-	if subCategoryType == requirementSubCategory && (itemType == semverMinorUpgradeItem || itemType == additionItem) {
-		return true
-	}
-
-	return subCategoryType == transitiveSubCategory && (itemType == semverPatchDowngradeItem || itemType == removalItem)
-}
-
-func getMarkdownCategoryType(change *shared.PackageChange) (markdownCategory, markdownSubCategory) {
-	category := prodUsageCategory // By default, for security, better than defining a package as dev-only while it's not
-	if change.Package.IsDevOnly() {
-		category = devOnlyUsageCategory
-	}
-
-	if change.Package.IsRootRequirement() || change.Package.IsRootDevRequirement() {
-		return category, requirementSubCategory
-	} else {
-		return category, transitiveSubCategory
-	}
-}
-
-func getMarkdownItemType(change *shared.PackageChange) markdownItem { //nolint:cyclop,lll // 13 vs 10 allowed, but 13 actual cases
-	switch change.Operation.Name {
-	// - UNKNOWN_UPDATE
-	// - UNKNOWN_UPDATE
-	case shared.UnknownUpdateOperation:
-		return unknownUpdateItem
-	// - SEMVER_MAJOR_UPGRADE
-	// - SEMVER_MINOR_UPGRADE
-	// - SEMVER_PATCH_UPGRADE
-	case shared.UpgradeOperation:
-		//nolint:exhaustive // SemverExtra + SemverUnknown + SemverNoUpdate managed as unknownUpdateItem !
-		switch change.Operation.SemverType {
-		case shared.SemverMajorUpdate:
-			return semverMajorUpgradeItem
-		case shared.SemverMinorUpdate:
-			return semverMinorUpgradeItem
-		case shared.SemverPatchUpdate:
-			return semverPatchUpgradeItem
-		}
-	// - SEMVER_MAJOR_DOWNGRADE
-	// - SEMVER_MINOR_DOWNGRADE
-	// - SEMVER_PATCH_DOWNGRADE
-	case shared.DowngradeOperation:
-		//nolint:exhaustive // SemverExtra + SemverUnknown + SemverNoUpdate managed as unknownUpdateItem !
-		switch change.Operation.SemverType {
-		case shared.SemverMajorUpdate:
-			return semverMajorDowngradeItem
-		case shared.SemverMinorUpdate:
-			return semverMinorDowngradeItem
-		case shared.SemverPatchUpdate:
-			return semverPatchDowngradeItem
-		}
-	// - REMOVAL
-	case shared.RemovalOperation:
-		return removalItem
-	// - ADDITION
-	case shared.AdditionOperation:
-		return additionItem
-	// - SAME
-	case shared.NoChangeOperation:
-		return sameItem
-	}
-
-	return unknownUpdateItem // Fallback on unknown
-}
-
-func _debugPackageList(
-	sectionType markdownSection,
-	categoryType markdownCategory,
-	subCategoryType markdownSubCategory,
-	itemType markdownItem,
-	change *shared.PackageChange,
+func processCategory(
+	builder *markdown.Builder,
+	subCategoriesMap subCategoriesMap,
+	categoryName markdownCategory,
+	openedDetails bool,
 ) {
-	expectedTypeKey := strings.ToLower(string(sectionType)) +
-		"-" + strings.ToLower(string(categoryType)) +
-		"-" + strings.ToLower(string(subCategoryType))
-	if !change.Package.IsDevOnly() && change.Package.IsRootDevRequirement() {
-		expectedTypeKey += "+dev_req"
+	fmt.Fprintln(os.Stderr, "Processing category:", categoryName)
+
+	noChangePkgList, otherChangePkgList := splitItemList(subCategoriesMap)
+
+	builder.Header(getCategoryHeaderFor(categoryName), categoryHeaderLevel, 0)
+	builder.Details(
+		buildSectionSummaryMrk(subCategoriesMap),
+		func(builder *markdown.Builder, indentDepth int) {
+			if len(otherChangePkgList) > 0 {
+				processPkgList(builder, otherChangePkgList, guessShortestPkgRowMode(otherChangePkgList), indentDepth)
+			}
+
+			if len(noChangePkgList) > 0 {
+				builder.Details(
+					"Unchanged packages 🟰<sup>"+strconv.Itoa(len(noChangePkgList))+"</sup>",
+					func(builder *markdown.Builder, indentDepth int) {
+						processPkgList(builder, noChangePkgList, versionOnlyPkgRowMode, indentDepth)
+					},
+					false, // Always closed by default (goal is to avoid polluting other changes)
+					indentDepth,
+				)
+			}
+		},
+		openedDetails,
+		0,
+	)
+}
+
+func processPkgList(builder *markdown.Builder, pkgList pkgList, tableMode pkgRowMode, indentDepth int) {
+	builder.HTMLTable(
+		func(yield func([]string) bool) {
+			for _, item := range pkgList {
+				if !yield(buildItemMrkRowCells(item, tableMode)) {
+					return
+				}
+			}
+		},
+		indentDepth,
+	)
+}
+
+func buildItemMrkRowCells(item *shared.PackageChange, tableMode pkgRowMode) []string {
+	cellList := []string{
+		buildPackageNameHTMLCell(item.Package),
 	}
 
-	expectedTypeKey += "/" + string(itemType)
-	if itemType == unknownUpdateItem && change.Operation.SemverType == shared.SemverExtraUpdate {
-		expectedTypeKey += "+SEMVER_EXTRA"
-	} else if change.Package.IsAbandoned() {
-		expectedTypeKey += "+ABANDONED"
+	pkgVersionCell := buildPackageVersionHTMLCell(item.Package.GetVersion())
+
+	switch tableMode {
+	case versionOnlyPkgRowMode:
+		cellList = append(cellList, pkgVersionCell)
+
+	case withOperationPkgRowMode:
+		operationCell := buildOperationHTMLCell(item.Operation, 0)
+		if item.Operation.Name != shared.AdditionOperation {
+			cellList = append(cellList, pkgVersionCell, operationCell)
+		} else {
+			cellList = append(cellList, operationCell, pkgVersionCell)
+		}
+
+	case fullPkgRowMode:
+		cellList = buildItemMrkFullPkgRowCells(item, cellList, pkgVersionCell)
+
+	default:
+		panic("Unmanaged table mode:" + strconv.Itoa(int(tableMode)))
 	}
 
-	if change.Package.GetName() != expectedTypeKey {
-		fmt.Fprintln(os.Stderr, "package mismatch: got", change.Package.GetName(), ", want", expectedTypeKey)
+	return cellList
+}
+
+func buildItemMrkFullPkgRowCells(item *shared.PackageChange, cellList []string, pkgVersionCell string) []string {
+	if item.Operation.Name != shared.AdditionOperation { // Version will be printed at the end for added package !
+		cellList = append(cellList, pkgVersionCell)
 	}
+
+	colspan := 0
+	if shared.NoChangeOperation == item.Operation.Name ||
+		shared.AdditionOperation == item.Operation.Name ||
+		shared.RemovalOperation == item.Operation.Name {
+		colspan = 2
+	}
+
+	cellList = append(cellList, buildOperationHTMLCell(item.Operation, colspan))
+
+	switch item.Operation.Name { //nolint:exhaustive // Only those cases should be handled here !
+	case shared.AdditionOperation:
+		cellList = append(cellList, pkgVersionCell)
+	case shared.UnknownUpdateOperation, shared.UpgradeOperation, shared.DowngradeOperation:
+		cellList = append(cellList, buildPackageVersionHTMLCell(item.PreviousVersion))
+	}
+
+	return cellList
+}
+
+func buildOperationHTMLCell(operation shared.Operation, colspan int) string {
+	opColspanDirective := ""
+	if colspan > 1 {
+		opColspanDirective = fmt.Sprintf(" colspan=\"%d\"", colspan)
+	}
+
+	return "<td align=\"center\"" + opColspanDirective + ">" + getOperationSymbol(operation) + "</td>"
+}
+
+func buildPackageVersionHTMLCell(version shared.PkgVersion) string {
+	return "<td align=\"right\">" + version.Label + "</td>"
+}
+
+func buildPackageNameHTMLCell(pkg shared.PkgWrapper) string {
+	pkgTitle := pkg.GetName()
+	if pkg.GetLink() != "" {
+		pkgTitle = "<a href=\"" + pkg.GetLink() + "\">" + pkgTitle + "</a>"
+	}
+	// Prepend package type symbol
+	pkgTitle = "<sup>" + getPackageSymbol(pkg) + "</sup>" + pkgTitle
+	// Append abandoned symbol
+	if pkg.IsAbandoned() {
+		pkgTitle += "💀"
+	}
+
+	return "<td align=\"left\">" + pkgTitle + "</td>"
 }
