4175227f286ff4c33ad692d795677019ad6481ef
Yoanm <4410697+yoanm@users.noreply.github.com>
2026-05-06 10:38:00 +0200

    Fix benchmark output

--- summary/main_benchmark_test.go
+++ summary/main_benchmark_test.go
@@ -1,6 +1,8 @@
 package summary_test
 
 import (
+	"io"
+	"log"
 	"log/slog"
 	"testing"
 
@@ -14,7 +16,7 @@ func BenchmarkGenerateForChanges(b *testing.B) {
 		for pb.Next() { // pb.Next() returns false when the benchmark should stop
 			// Enable debug logs to check speed and allocation even at this level
 			slog.SetLogLoggerLevel(slog.LevelDebug)
-
+			log.SetOutput(io.Discard) // But drop output to avoid parsing issues later
 			_ = summary.GenerateForChanges(_integrationFullChanges)
 		}
 	})
